d3ee335e2cc688f822b492e41e4be0063f022757
Jason M. Goncalves <jgoncalves@protonmail.com>
2018-04-28 09:41:11 -0400

    set up initial react files using create-react-app

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,10 @@
+// sets up the reusable Footer Component
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => (
+	<footer className='footer'>
+		<p>©2018 Jason Goncalves</p>
+	</footer>
+
+);
